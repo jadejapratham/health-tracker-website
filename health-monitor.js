33f65f7561b40b85f4b6a61520bc7d34aa7ef842
jadejapratham <jadejapratham08@gmail.com>
2025-06-28 13:58:19 +0530

    health-monitor.js

--- health-monitor.js
+++ health-monitor.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Health Monitor Page Loaded");
 });
 
+// if login/signup model using 
 const modal = document.getElementById("loginModal");
 const openModalBtn = document.getElementById("openModalBtn");
 const closeModal = document.querySelector(".close");
